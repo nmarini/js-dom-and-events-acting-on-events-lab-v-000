c38bb7718a3590cf56335f7ca5c9001f20ff0ad2
Nicolas Marini <nmarini44@gmail.com>
2019-05-10 19:30:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = $('input')
+// retrieveEmployeeInformation(): 
+// This retrieves the employee information from the input. 
+// It returns a string equal to the value in the input.
+function retrieveEmployeeInformation(){
+  return 
+}
+
+// addNewElementAsLi(): 
+// This function takes the value retrieved from the previous 
+// retrieveEmployeeInformation function and adds that string to a new list 
+// element which is appended to the ul with class "employee-list".
+
+// addNewLiOnClick(): 
+// This adds the employee name as an li element on click and clears the input value.
+
+// clearEmployeeListOnLinkClick(): 
+// This empties out the employee list when "Clear employee list" button clicked.
