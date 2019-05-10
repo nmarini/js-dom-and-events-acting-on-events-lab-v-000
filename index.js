04cd877b3c35baeb2d966f81b8d398f70141c94b
Nicolas Marini <nmarini44@gmail.com>
2019-05-10 20:03:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function addNewElementAsLi(){
 function addNewLiOnClick(){
   let submit = document.querySelector('input[type="submit"]')
   submit.addEventListener('click', function(e){
-
+    addNewElementAsLi();
+    input.value = ""
   })
 
 }
