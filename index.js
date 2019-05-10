51d5ae40713189f4451b476dd4b5e3294f163696
Nicolas Marini <nmarini44@gmail.com>
2019-05-10 20:22:02 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const ul = document.querySelector('.employee-list')
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi(){
+  let li = document.createElement("LI")
+  li.innerHTML = retrieveEmployeeInformation()
+  ul.appendChild(li)
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(e){
+    addNewElementAsLi();
+    input.value = ""
+  })
+
+}
+
+
+function clearEmployeeListOnLinkClick(){
+    let clear = document.querySelector('a')
+    clear.addEventListener('click', function(e){
+      ul.innerHTML = '';
+    })
+}
